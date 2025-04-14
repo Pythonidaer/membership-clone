ee7c256c6eba9fa5d5e7ad513ea6f1deb28b498c
Jonathan Hammond <codefolio.work@gmail.com>
2025-04-14 10:00:48 -0400

    deleted original filter example in lieu of my own

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-// import FilterList from '../components/FilterList';
 // import TestFilterList from '../components/TestFilterList';
-// import MyFilterList from '../components/MyFilterList';
-import LoginForm from '../components/LoginForm';
+import MyFilterList from '../components/MyFilterList';
+// import LoginForm from '../components/LoginForm';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <LoginForm />
+      <MyFilterList />
+      {/* <LoginForm /> */}
     </main>
   );
 }
